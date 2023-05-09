5fb03ec62cef3f208929e3dfa089bfb3e5925220
Aiturgan0 <118384435+Aiturgan0@users.noreply.github.com>
2023-05-09 11:34:13 +0300

    Add files via upload

---
+++ acod.js
@@ -0,0 +1,14 @@
+function changeText() {
+    var button = document.getElementById("myButton");
+    if (button.innerHTML === "Order now") {
+      button.innerHTML = "Ordered!";
+    } else {
+      button.innerHTML = "Order now";
+    }
+  }
+  
+
+
+  
+ 
+
